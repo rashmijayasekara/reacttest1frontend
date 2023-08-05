1e714356143e154243c4eab7489f2f7df2b5e94f
Rashmi <rashmijayasekara00@gmail.com>
2023-08-05 20:02:39 +0530

    POST data into database AXIO POST

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,6 +1,10 @@
+import axios from 'axios';
 import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function AddUser() {
+    let navigate=useNavigate()
+
     const [user, setUser]=useState({
         name:"",
         username:"",
@@ -13,12 +17,20 @@ export default function AddUser() {
         setUser({...user,[e.target.name]:e.target.value})
     }
 
+    const onSubmit=async (e)=>{
+        e.preventDefault();
+        await axios.post("http://localhost:8080/user",user)
+        navigate("/")
+
+    }
+
 
   return (
     <div className='container'>
       <div className='row'>
         <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
             <h2 className='text-center m-4'>Register User</h2>
+            <form onSubmit={(e)=>onSubmit(e)}>
             <div className='mb-3'>
                 <label htmlFor='Name' className='form-label'>Name</label>
                 <input 
@@ -54,7 +66,7 @@ export default function AddUser() {
             </div>
             <button type='submit' className='btn btn-outline-primary'>Submit</button>
             <button type='cancel' className='btn btn-outline-danger mx-2'>Cancel</button>
-
+            </form>
         </div>
       </div>
     </div>
