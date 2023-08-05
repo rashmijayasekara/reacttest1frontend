7d83715c93f42e49b5e197f3156ae0fecb017411
Rashmi <rashmijayasekara00@gmail.com>
2023-08-05 18:59:22 +0530

    Display information using AXIOS

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,9 +22,9 @@ export default function Home() {
             <thead>
                 <tr>
                     <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">User Name</th>
+                    <th scope="col">Email</th>
                 </tr>
             </thead>
             <tbody>
