1b0f9d44f45290a66bfe9d76ed623e716b97d4ff
Rashmi <rashmijayasekara00@gmail.com>
2023-08-05 18:18:48 +0530

    Add Navbar

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
+
 export default function Navbar() {
   return (
     <div>
-      
-
-      
-
-
-
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Navbar w/ text</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+            </div>
+        </nav>
     </div>
   );
 }
