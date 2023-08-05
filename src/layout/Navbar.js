3fc7a4c446c500c086791e68574c336dc30bc5df
Rashmi <rashmijayasekara00@gmail.com>
2023-08-05 19:19:12 +0530

    configure Routing using React-Router-DOM

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 
 export default function Navbar() {
@@ -12,7 +13,7 @@ export default function Navbar() {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <button className='btn btn-outline-light'>Add User</button>
+                <Link className='btn btn-outline-light' to="/adduser">Add User</Link>
             </div>
         </nav>
     </div>
