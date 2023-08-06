f38954330daf9f4ec66c397107b671a53b2a56f0
Rashmi <rashmijayasekara00@gmail.com>
2023-08-06 06:38:11 +0530

    Delete user using AXIOS

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import { Link, useParams } from 'react-router-dom';
 
 export default function Home() {
 
     const [users, setUsers]=useState([]);
+
+    const {id}=useParams();
 
     useEffect(() => {
         loadUsers();
@@ -14,6 +17,12 @@ export default function Home() {
         const result=await axios.get("http://localhost:8080/users");
         setUsers(result.data);
     }
+
+    const deleteUser=async (id)=>{
+        await axios.delete(`http://localhost:8080/user/${id}`)
+        loadUsers()
+    }
+
 
   return (
     <div className='container'>
@@ -37,8 +46,8 @@ export default function Home() {
                             <td>{user.email}</td>
                             <td>
                                 <button className='btn btn-primary mx-2'>View</button>
-                                <button className='btn btn-outline-primary mx-2'>Edit</button>
-                                <button className='btn btn-danger mx-2'>Delete</button>
+                                <Link className='btn btn-outline-primary mx-2' to={`/edituser/${user.id}`}>Edit</Link>
+                                <button className='btn btn-danger mx-2' onClick={()=>deleteUser(user.id)}>Delete</button>
                             </td>
                         </tr>
                     ))
